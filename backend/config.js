d88578d41934b9cfb98a97e154efdc07be15881f
Daria Lazarchuk <lazarchukdarya@gmail.com>
2023-03-22 22:59:52 +0300

    fix: change port 3000

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { PORT = '3001' } = process.env;
+const { PORT = '3000' } = process.env;
 const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 module.exports = {
