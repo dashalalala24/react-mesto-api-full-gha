be450eed9f0e2a0ea1bb4e70e90c1ecce26c04d3
Daria Lazarchuk <lazarchukdarya@gmail.com>
2023-04-02 16:50:10 +0300

    refactor: auth singlequotes

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,25 +1,25 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
-const UnauthorizedError = require("../errors/UnauthorizedError");
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith("Bearer ")) {
-    next(new UnauthorizedError("Необходима авторизация"));
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    next(new UnauthorizedError('Необходима авторизация'));
   }
 
-  const token = authorization.replace("Bearer ", "");
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
     payload = jwt.verify(
       token,
-      NODE_ENV === "production" ? JWT_SECRET : "secret-key"
+      NODE_ENV === production ? JWT_SECRET : 'secret-key',
     );
   } catch {
-    next(new UnauthorizedError("Необходима авторизация"));
+    next(new UnauthorizedError('Необходима авторизация'));
   }
 
   req.user = payload;
